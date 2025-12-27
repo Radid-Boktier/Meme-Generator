9bc4880b68a6c79a4e9eafb295ab14f98b25bd1b
Radid Ahammed <radidboktier823@gmail.com>
2025-12-27 14:35:54 +0600

    feat: fetch meme data with useEffect and generate random meme images

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import GetMemem from './GetMeme';
 import TextInput from './TextInput';
 
@@ -8,15 +8,33 @@ export default function Main() {
     bottomText: 'Walk into Mordor',
     imgUrl: 'http://i.imgflip.com/1bij.jpg',
   });
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes')
+      .then((res) => res.json())
+      .then((resData) => setData(resData.data.memes));
+  }, []);
+
+  // console.log(data);
   function handleChange(event) {
     const { value, name } = event.target;
-    // console.log(value, name);
+    console.log(value, name);
     setMemeInfo((prevMeme) => ({
       ...prevMeme,
       [name]: value,
     }));
   }
 
+  function getImage() {
+    const index = Math.floor(Math.random() * data.length);
+    const img = data[index].url;
+    // console.log(index, img);
+    setMemeInfo((prevMeme) => ({
+      ...prevMeme,
+      imgUrl: img,
+    }));
+  }
   return (
     <main>
       <div className="form">
@@ -40,7 +58,9 @@ export default function Main() {
           handleChange={handleChange}
           value={memeInfo.bottomText}
         />
-        <button className="getImageButton">Get a new meme image 🖼</button>
+        <button className="getImageButton" onClick={getImage}>
+          Get a new meme image 🖼
+        </button>
       </div>
       <GetMemem memeInfo={memeInfo} />
     </main>
